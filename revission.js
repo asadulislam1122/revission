c916ec2e38fdcf1c12d3a3c42a07a506c70f54d1
asadulislam <asadulislam0288@gmail.com>
2026-06-17 11:11:53 +0600

    first commit

---
+++ revission.js
@@ -0,0 +1,39 @@
+const price = 5200;
+
+console.log(price);
+
+const name = "kie kie ali khan";
+
+console.log(name);
+
+const isPore = false;
+
+console.log(isPore);
+
+const friends = ["asadul", "sabbir", "sakib", "badsa"];
+
+console.log(friends[2]);
+
+// object
+
+const student = { name: "md asadullah", age: 22 };
+console.log(student.age);
+
+if (price > 500) {
+  console.log("ami tomake taka dite parbo karon tumi boroloks");
+} else {
+  console.log("ami tomake taka dite parbo na karon tumi garib");
+}
+
+// loop
+let pushup = 0;
+while (pushup < 100) {
+  console.log("ami pushup kortesi", pushup);
+  pushup++;
+}
+
+// for loop
+
+for (let i = 0; i < 100; i++) {
+  console.log("ami for loop er moddhe", i);
+}
