db87afeeb0d21a9206e6030f3fb972cc4ede1acb
asadulislam <asadulislam0288@gmail.com>
2026-06-22 18:58:05 +0600

    added by paktish

---
+++ paktish.js
@@ -0,0 +1,13 @@
+// const fruits = ["apple", "banana", "komola", "anaros"];
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+//   console.log(fruits[i]);
+// }
+
+const name = ["asadul", "sabbir", "masud", "sakib", "badsa", "raja"];
+
+for (let i = 0; i < name.length; i++) {
+  // console.log(i);
+  console.log(name[i]);
+}
