4f657869589df35042f8cdb7838f656c1a851979
asadulislam <asadulislam0288@gmail.com>
2026-07-08 14:41:36 +0600

    price

---
+++ price.js
@@ -0,0 +1,17 @@
+function maxProfit(prices) {
+  let maxProfit = 0;
+
+  for (let i = 0; i < prices.length; i++) {
+    for (let j = i + 1; j < prices.length; j++) {
+      const profit = prices[j] - prices[i];
+
+      if (profit > maxProfit) {
+        maxProfit = profit;
+      }
+    }
+  }
+
+  return maxProfit;
+}
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
