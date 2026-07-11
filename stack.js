69cc634ce984e1c0c4cd5ad624d62b0253ae9c3c
asadulislam <asadulislam0288@gmail.com>
2026-07-11 21:43:14 +0600

    added by push and pop

---
+++ stack.js
@@ -0,0 +1,40 @@
+// // const stack = [];
+
+// stack.push("(");
+// stack.push("[");
+
+// console.log(stack);
+
+// // const stack = [];
+
+// stack.push("(");
+// stack.push("[");
+// stack.push("{");
+
+// console.log(stack);
+
+// console.log(stack.pop());
+
+// console.log(stack);
+
+// const stack = [];
+
+// stack.push("()");
+
+// console.log(stack);
+
+// // const stack = [];
+// stack.push("(");
+// stack.push("[");
+// stack.push("{");
+
+// stack.pop();
+// stack.pop();
+
+// // console.log(stack);
+
+const stack = [];
+stack.push("(");
+stack.pop();
+
+console.log(stack);
