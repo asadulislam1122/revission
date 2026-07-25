31a83ad9c48ed5b02de10eed90b0134d7b7d2bcd
asadulislam <asadulislam0288@gmail.com>
2026-07-25 23:16:54 +0600

    added str

--- revarse.js
+++ revarse.js
@@ -1,25 +1,41 @@
-function reverseString(str) {
-  let reversed = "";
+// function reverseString(str) {
+//   let reversed = "";
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     reversed += str[i];
+//   }
+//   return reversed;
+// }
+
+// console.log(reverseString("Hello World"));
+
+// // Second problem
+
+// function reverseWords(str) {
+//   return str.split(" ").reverse().join(" ");
+// }
+
+// console.log(reverseWords("Hello World from JavaScript"));
+
+// //  Third problem
+
+// function revarsed(str) {
+//   return str.split("").reverse().join("");
+// }
+
+// console.log(revarsed("Hello World"));
+
+//  Fourth problem
+
+function rearseString(str) {
+  let revarsed = "";
+
   for (let i = str.length - 1; i >= 0; i--) {
-    reversed += str[i];
+    revarsed += str[i];
   }
-  return reversed;
+
+  return revarsed;
 }
 
-console.log(reverseString("Hello World"));
+console.log(rearseString("Hello JavaScript"));
 
-// Second problem
-
-function reverseWords(str) {
-  return str.split(" ").reverse().join(" ");
-}
-
-console.log(reverseWords("Hello World from JavaScript"));
-
-//  Third problem
-
-function revarsed(str) {
-  return str.split("").reverse().join("");
-}
-
-console.log(revarsed("Hello World"));
+// fifth problerm
