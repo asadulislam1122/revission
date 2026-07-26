81eb6be48f9090cf4cbb48e95a3ad9eadf132f78
asadulislam <asadulislam0288@gmail.com>
2026-07-26 22:37:06 +0600

    added by s

--- revarse.js
+++ revarse.js
@@ -1,28 +1,28 @@
-// function reverseString(str) {
-//   let reversed = "";
-//   for (let i = str.length - 1; i >= 0; i--) {
-//     reversed += str[i];
-//   }
-//   return reversed;
-// }
+function reverseString(str) {
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
 
-// console.log(reverseString("Hello World"));
+console.log(reverseString("Hello World"));
 
-// // Second problem
+// Second problem
 
-// function reverseWords(str) {
-//   return str.split(" ").reverse().join(" ");
-// }
+function reverseWords(str) {
+  return str.split(" ").reverse().join(" ");
+}
 
-// console.log(reverseWords("Hello World from JavaScript"));
+console.log(reverseWords("Hello World from JavaScript"));
 
-// //  Third problem
+//  Third problem
 
-// function revarsed(str) {
-//   return str.split("").reverse().join("");
-// }
+function revarsed(str) {
+  return str.split("").reverse().join("");
+}
 
-// console.log(revarsed("Hello World"));
+console.log(revarsed("Hello World"));
 
 //  Fourth problem
 
