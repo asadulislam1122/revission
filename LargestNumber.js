26f1340c6e6dae5411eab229967c5fad222ba113
asadulislam <asadulislam0288@gmail.com>
2026-07-28 12:54:08 +0600

    added largestNumber

---
+++ LargestNumber.js
@@ -0,0 +1,33 @@
+function secondlargestNumber(arr) {
+  let largest = -Infinity;
+  let secondLargest = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      secondLargest = largest;
+      largest = arr[i];
+    }
+  }
+  return secondLargest;
+}
+
+// console.log(secondlargestNumber([50, 40, 30, 20]));
+
+function secondlargestNumber(arr) {
+  let largest = -Infinity;
+  let secondLargest = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      secondLargest = largest;
+      largest = arr[i];
+    } else if (arr[i] > secondLargest && arr[i] < largest) {
+      secondLargest = arr[i];
+    }
+  }
+
+  return secondLargest;
+}
+
+console.log(secondlargestNumber([50, 40, 30, 20])); // 40
+console.log(secondlargestNumber([10, 20, 30, 40, 50])); // 40
